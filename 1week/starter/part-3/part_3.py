bc9239ccd3272bebfd1a98de8940a116a2e462ce
Derrick Furtado <derrick.furtado10@gmail.com>
2024-03-20 14:29:15 -0600

    completed part 3

--- 1week/starter/part-3/part_3.py
+++ 1week/starter/part-3/part_3.py
@@ -1,15 +1,60 @@
 my_book = {
-    "title": "Then Hunger Games",
+    "title": "The Hunger Games",
     "author": "Suzanne Collins",
     "year": 2008,
     "rating": 4.32,
     "pages": 374
 }
 
+my_book_shelf = [
+    {
+    "title": "To Kill a Mockingbird",
+    "author": "Harper Lee",
+    "year": 1960,
+    "rating": 4.27,
+    "pages": 324
+},{
+    "title": "1984",
+    "author": "George Orwell",
+    "year": 1949,
+    "rating": 4.17,
+    "pages": 328
+},{
+    "title": "Harry Potter and the Sorcerer's Stone",
+    "author": "J.K. Rowling",
+    "year": 1997,
+    "rating": 4.47,
+    "pages": 309
+},{
+    "title": "The Great Gatsby",
+    "author": "F. Scott Fitzgerald",
+    "year": 1925,
+    "rating": 3.91,
+    "pages": 180
+},{
+    "title": "Pride and Prejudice",
+    "author": "Jane Austen",
+    "year": 1813,
+    "rating": 4.25,
+    "pages": 279
+}
+]
+
+
 # Follow the instructions in this part of the project. Define and flesh out your function below, which should accept a dictionary as an argument when called, and return a string of the info in that book-dictionary in a user-friendly readable format.
 
 # Code below
 
+def describe_book(book):
+    title = book["title"]
+    author = book["author"]
+    year = book["year"]
+    rating = book["rating"]
+    pages = book["pages"]
+
+    return f"This book is called {title} written by {author} and published in the year {year}. With an average rating of {rating}, this book has a total length of {pages} pages."
+
+print(describe_book(my_book))
 
 
 
@@ -17,7 +62,83 @@ my_book = {
 
 # Code below
 
+def show_authors(book):
+    author = book["author"]
+    return author
+
+def list_publish_date(book):
+    year = book["year"]
+    return year
+
+def show_title(book):
+    title = book["title"]
+    return title
+
+def show_rating(book):
+    rating = book["rating"]
+    return rating
+
+def show_page_count(book):
+    pages = book["pages"]
+    return pages
+
+
+show_authors(my_book)
+list_publish_date(my_book)
+show_title(my_book)
+show_rating(my_book)
+show_page_count(my_book)
+
+
 
 # Finally, create at least three new functions that might be useful as we expand our home library app. Perhaps thing of a way you could accept additional arguments when the function is called? Also, imagine you have a list filled with dictionaries like above.
 
 # Code below
+
+def show_longest_book(shelf):
+    largestBook = 0
+    bookTitle = ""
+    for book in shelf:
+        title = show_title(book)
+        pages = show_page_count(book)
+        if pages > largestBook:
+            largestBook = pages
+            bookTitle = title
+        else: continue
+    return print(f"{bookTitle} is the largest book with {largestBook} pages.")
+        
+def show_oldest_book(shelf):
+    oldestBook = 2300
+    bookTitle = ""
+    for book in shelf:
+        title = show_title(book)
+        year = list_publish_date(book)
+        if year < oldestBook:
+            oldestBook = year
+            bookTitle = title
+        else: continue
+    return print(f"{bookTitle} is the oldest book published in the year {oldestBook}.")
+
+def top_rated_book(shelf):
+    topRating = 0
+    bookTitle = ""
+    for book in shelf:
+        title = show_title(book)
+        rating = show_rating(book)
+        if rating > topRating:
+            topRating = rating
+            bookTitle = title
+    return print(f"{bookTitle} is the top rated book at a rating of {topRating}.")
+        
+
+def describe_all_books(shelf):
+    printer = []
+    for book in shelf:
+        printer.append(describe_book(book))
+        print(describe_book(book))
+    return printer
+
+
+
+
+
---
+++ 1week/starter/part-3/practice3.py
@@ -0,0 +1,126 @@
+print("======================")
+
+
+myList = [1,2,3,4,5,6,7,8,9,10]
+
+print("My list before: ", myList)
+
+
+myList.insert(3,11)
+
+print("My list after inserting 11 value into 3rd index: ", myList)
+
+print("======================")
+
+def hello():
+    x = "hello"
+    return x
+
+
+print(hello())
+
+print("======================")
+
+
+def say_hello (name="default"):                 # add in this syntax to avoid errors and have a fallback variable
+    print(f"Hey {name}!. How are you?")
+
+
+say_hello("John")
+say_hello()
+
+
+
+
+
+
+print("======================")
+
+
+def sum_num(num1, num2):
+    print(num1+num2)
+    return num1 + num2
+
+
+
+
+# def sum_num(num1, num2):                      #fix function
+#     num1 = int(num1)
+#     num2 = int(num2)
+#     print(num1+num2)
+#     return num1 + num2
+
+
+# return correctly with integers
+
+sum_num(20,30)
+
+
+# return as string
+sum_num('20','30')
+
+
+
+
+
+print("======================")
+
+
+
+def is_prime(n):
+    for i in range(2,n):
+        # print(f"i is {i} and n is {n} and the remainder is: ", n % i)
+        if n % i == 0:
+            return
+    return n
+
+
+primeNumbers = []
+
+def sort_prime_num(start, stop):
+    for i in range(start,stop):
+        if is_prime(i) is not None:
+            primeNumbers.append(i)
+        else:
+            continue
+
+
+# sort_prime_num(0, 100)
+# sort_prime_num(100, 1000)
+# sort_prime_num(1000, 10000)
+# sort_prime_num(10000, 100000)
+
+print("The number of primes is: ", len(primeNumbers))
+
+
+
+
+
+print("======================")
+
+crypto_prices = (("Bitcoin", 1000), ("Ether", 3300), ("XRP", 45))
+
+for x, y in crypto_prices:
+    print(x)
+    
+for x, y in crypto_prices:
+    print(y)
+
+def check_highest_price(tup):
+    highest_price = 0
+    highest_coin = 0
+    for name, price in tup:
+        if price > highest_price:
+            highest_price = price
+            highest_coin = name
+        else:
+            continue
+    return (highest_coin, highest_price)
+        
+
+print("highest priced coin is: ", check_highest_price(crypto_prices))
+
+print("======================")
+
+
+
