dad7c263f2be4b019c8332bbf4eb7e3a210141b2
Derrick Furtado <derrick.furtado10@gmail.com>
2024-03-22 11:08:09 -0600

    finished part 3

--- 1week/part-4/part_4.py
+++ 1week/part-4/part_4.py
@@ -3,15 +3,50 @@
 ## Create five input statements to gather user's book they want to input to the system. After that be sure to turn it into a function.
 
 # Code here
+def capture_book():
+    bookName = input("What is the book's title?")
+    return bookName
+
+def capture_author():
+    author = input("Who is the book's author?")
+    return author
+
+# def capture_year():
+#     year = input("when was the book published?")
+#     return year
+
+# def capture_rating():
+#     rating = input("What is the book's rating")
+#     return rating
+
+# def capture_pages():
+#     pages = input("How many pages does the book have?")
+#     return pages
+
 
 
 ### Step 2 - Type conversion
 
-## Now convert the proper data-types front strings to either floats or ints depending on what it is. Feel free to comment out your old function so you don't get an error, or copy/paste and give it a new name.
+## Now convert the proper data-types from strings to either floats or ints depending on what it is. Feel free to comment out your old function so you don't get an error, or copy/paste and give it a new name.
 
 # Code here
 
+def capture_year():
+    year = input("When was the book published? ")
+    return int(year)
 
+def capture_rating():
+    rating = input("What is the book's rating ")
+    return float(rating)
+
+def capture_pages():
+    pages = input("How many pages does the book have? ")
+    return int(pages)
+
+
+print("year type is: ", type(capture_year()))
+print("rating type is: ", type(capture_rating()))
+print("pages type is: ", type(capture_pages()))
 
 ### Step 3 - Error handling
 
---
+++ 1week/part-4/practice4.py
@@ -0,0 +1,74 @@
+def my_basket(*args,**kwargs):
+    if 'fruit' in kwargs:
+        print(f"My basket contains {kwargs['fruit']} fruits.")
+    elif 'veggie' in kwargs:
+        print(f"My basket contains {kwargs['veggie']} vegetables.")
+
+
+my_basket(veggie="cucumber")
+
+
+print("===================================")
+
+
+def my_basket2(*args,**kwargs):
+    print(args)
+    print(kwargs)
+
+my_basket2(30,40,10,fruit="orange", drinks="Monster Energy")
+
+print("===================================")
+
+
+
+def even_capitalized(string):
+    new_string = ""
+    string = string.replace(' ','')
+    string = string.replace('.','')
+    for x in range (0,len(string)):
+        if x % 2 == 0:
+            new_string = new_string + string[x].upper()
+        else:
+            new_string = new_string + string[x].lower()
+        
+    return new_string
+        
+even_capitalized("test string")
+even_capitalized("This string will test this function further. It adds additional sentences too.")
+
+
+
+print("===================================")
+
+def third(num):
+    return num * 3
+
+my_nums = [1,2,3,4,5,6,7,8,9,10]
+
+for x in map(third, my_nums):
+    print(x)
+
+func = list(map(third, my_nums))
+
+print(func)
+
+
+
+print("===================================")
+
+def check_even(num):
+    return num % 2 is not 0
+
+for x in filter(check_even,func):
+    print(x)
+
+check = list(filter(check_even,func))
+print(check)
+
+
+
+
+
+print("===================================")
+
+
