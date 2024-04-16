2ffe989de095275b0bda01cea66a4f5c15ca6317
Derrick Furtado <derrick.furtado10@gmail.com>
2024-04-15 18:18:27 -0600

    finished 4-1. Wrapping up 4-2, but hitting db table error

---
+++ 4week/model.py
@@ -0,0 +1,46 @@
+import os
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+
+
+class User(db.Model):
+
+    __tablename__ = "users"
+
+    id = db.Column(db.Integer, primary_key = True, autoincrement = True)
+    username = db.Column(db.String(255), unique = True, nullable = False)
+    password = db.Column(db.String(255), nullable = False)
+
+class Team(db.Model):
+
+    __tablename__ = "teams"
+
+    id = db.Column(db.Integer, primary_key = True, autoincrement = True)
+    team_name = db.Column(db.String(255), unique = True, nullable = False)
+    user_id = db.Column(db.Integer, db.ForeignKey("users.id"), nullable = False)
+
+class Project(db.Model):
+
+    __tablename__ = "projects"
+    
+    id = db.Column(db.Integer, primary_key = True, autoincrement = True)
+    project_name = db.Column(db.String(255), nullable = False)
+    description = db.Column(db.String(255), nullable = True)
+    completed = db.Column(db.Boolean, default = False)
+    team_id = db.Column(db.Integer, db.ForeignKey("teams.id"), nullable = False)
+
+
+
+def connect_to_db(app):
+    app.config["SQLALCHEMY_DATABASE_URI"] = os.environ["POSTGRES_URI"]
+    app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+    db.app = app
+    db.init_app(app)
+
+if __name__ == "__main__":
+    from flask import Flask
+    app = Flask(__name__)
+    connect_to_db(app)
+    print("We're connected to db ...")
