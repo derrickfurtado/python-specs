33c616a9edf5be2032127d4888fd0b4d31406164
Derrick Furtado <derrick.furtado10@gmail.com>
2024-04-17 19:09:06 -0600

    finished 4-4

---
+++ 4week/forms.py
@@ -0,0 +1,18 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField, SelectField, BooleanField, TextAreaField
+from wtforms.validators import DataRequired, Length
+
+class TeamForm(FlaskForm):
+    team_name = StringField("Team Name: ", validators=[DataRequired(), Length(min=4, max=255)])
+    submit = SubmitField("Add Team")
+
+
+class ProjectForm(FlaskForm):
+    project_name = StringField("Project Name: ", validators=[DataRequired(), Length(min=4, max=255)])
+    description = TextAreaField("Project Description: ", validators=[DataRequired(), Length(min=4, max=255)])
+    # completed = BooleanField("Has it been completed?")
+    # team_selection = SelectField("Which team will own this: ")
+    submit = SubmitField("Add Project")
+
+    # def update_teams(self, teams):
+    #     self.team_selection.choices = [(team.id, team.team_names) for team in teams]                # need to understand this one
---
+++ 4week/server.py
@@ -0,0 +1,57 @@
+from flask import Flask, render_template, flash, url_for, redirect
+from pdb import set_trace
+from forms import TeamForm, ProjectForm
+from key import app_key
+from model import User, Team, Project, connect_to_db
+
+
+
+app = Flask(__name__)
+
+app.secret_key = app_key    
+
+user_id = 1
+
+
+@app.route("/")
+def homepage():
+    team_form = TeamForm()
+    project_form = ProjectForm()
+    # project_form.update_teams(User.query.get(user_id).teams)                                          ############
+    return render_template("home.html", team_form = team_form, project_form = project_form)
+
+
+@app.route("/add_team", methods=["POST"])
+def add_team():
+    team_form = TeamForm()
+
+    if team_form.validate_on_submit():
+        print(f"TEAM DATA: {team_form.team_name.data}")
+        flash(f"{team_form.team_name.data} Team created!")
+    else:
+        flash("Form failed to validate on submit")
+    return redirect(url_for("homepage"))
+
+
+@app.route("/add_project", methods=["POST"])
+def add_project():
+    project_form = ProjectForm()
+    # project_form.update_teams(User.query.get(user_id).teams)              ###############
+
+    if project_form.validate_on_submit():
+        print(f"PROJECT DATA: {project_form.project_name.data}")
+        print(f"DESCRIPTION DATA: {project_form.description.data}")
+        flash(f"{project_form.project_name.data} Project Created!")
+    else:
+        flash("Form failed to validate on submit")
+    return redirect(url_for("homepage"))
+
+
+
+
+
+
+if __name__ == "__main__":
+    app.debug = "development"
+    # connect_to_db(app)                                            ##############
+    app.run(debug=True, port = 4040, host = "localhost")
